21c64b035b92d3bc21ad5fc0e3c25436980fc676
김정준 <k4j1j7@naver.com>
2023-11-26 23:33:25 +0900

    edit: 주소 검색 잘못된 Merge 수정

--- BusTeamProject/app/src/main/java/com/busteamproject/util/Util.java
+++ BusTeamProject/app/src/main/java/com/busteamproject/util/Util.java
@@ -1,5 +1,7 @@
 package com.busteamproject.util;
 
+import android.util.Log;
+import com.busteamproject.DTO.AddressInfoDTO;
 import com.busteamproject.DTO.BusLocationDTO;
 import com.busteamproject.DTO.BusStationInfo;
 import com.busteamproject.DTO.BusStationSearchList;
@@ -84,6 +86,28 @@ public class Util {
         Type typeList = new TypeToken<ArrayList<BusLocationDTO>>() {
         }.getType();
         return new Gson().fromJson(jsonArray.toString(), typeList);
+    }
+
+    public static List<AddressInfoDTO> parseAddressDocument(JSONObject jsonData) {
+        try {
+            List<AddressInfoDTO> addressInfoDTOList = new ArrayList<>();
+            JSONArray ja = jsonData.getJSONArray("documents");
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject tmp = (JSONObject) ja.get(i);
+                addressInfoDTOList.add(new AddressInfoDTO(
+                        tmp.get("address_name").toString()
+                        , tmp.get("address_type").toString()
+                        , tmp.get("x").toString()
+                        , tmp.get("y").toString()
+                        , (JSONObject) tmp.get("address")
+                ));
+                Log.d("document", addressInfoDTOList.get(i).toString());
+            }
+            return addressInfoDTOList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static List<BusStationSearchList> parseBusStationArrivalInfo(String jsonResult) {
